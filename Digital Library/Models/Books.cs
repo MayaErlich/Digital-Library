4427495afb236e0e32866cab985f03259a82a207
MayaDev423 <118324060+MayaDev423@users.noreply.github.com>
2025-03-08 12:20:33 -0500

    added models

---
+++ Digital Library/Models/Books.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Models
+{
+    internal class Books
+
+    {
+        public int BookID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int GenreID { get; set; }
+        public int Length { get; set; }
+    }
+}
---
+++ Digital Library/Models/Insights.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Digital_Library.Models
+{
+    internal class Insights
+    {
+        public int InsightID { get; set; }
+        public int AvgPages  { get; set; }
+        public string FavGenre { get; set; }
+     
+    }
+}
